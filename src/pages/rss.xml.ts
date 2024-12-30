eabffbe92a1753a14df3f716a3e24d2015aba46e
alfonsovillanueva <alfonso@villanuevagarcia.com>
2024-12-29 22:21:30 -0600

    Agregando RSS

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,27 @@
+import rss from '@astrojs/rss';
+import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+
+
+export const GET: APIRoute = async ({ params, request, site }) => {
+    const blogPosts = await getCollection('blog');
+  return rss({
+    stylesheet: '/styles/rss.xsl',
+    // `<title>` campo en el xml generado
+    title: 'Alfgow Blog',
+    // `<description>` campo en el xml generado
+    description: 'Guía de un humilde astronauta a las estrellas',
+    // Usa el "site" desde el contexto del endpoint
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: site ?? '',
+    // Array de `<item>`s en el xml generado
+    // Consulta la sección "Generando `items`" para ejemplos utilizando colecciones de contenido y glob imports
+    items: blogPosts.map(({data, slug}) => ({
+      title: data.title,
+      pubDate: data.date,
+      description: data.description,
+      link: `/posts/${slug}/`
+    })),
+    customData: `<language>es-mx</language>`,
+  });
+}
